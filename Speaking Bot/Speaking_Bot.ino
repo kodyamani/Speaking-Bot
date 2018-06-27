d908fd076c6b9fa219d2ebdeaad4017aafe64bcc
kodyamani <40610898+kodyamani@users.noreply.github.com>
2018-06-27 07:03:17 -0700

    Add files via upload

---
+++ Speaking Bot/Speaking_Bot.ino
@@ -0,0 +1,24 @@
+int Piezo = A0;
+int sensorValue = 0;
+int threshold = 10;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(Piezo);
+
+  Serial.println("Sensor Value: ");
+  Serial.println(sensorValue);
+
+  delay(2000);
+
+  if (sensorValue >= threshold) {
+    Serial.println("I hear you!");
+    delay(1000);
+  }
+
+  delay(100);
+  
+}
